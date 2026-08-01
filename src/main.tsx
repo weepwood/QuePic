1253600c121e0735990bbb755f1bf5e38df242be
weepwood <weepwood@outlook.com>
2026-08-02 00:29:51 +0800

    refactor: 将文件夹转文档并入主应用

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { BatchDocumentUploader } from './components/BatchDocumentUploader';
 import './styles.css';
 import './preview.css';
-import './batch-document.css';
 import './yuque-theme.css';
+import './batch-document.css';
+import './feature-enhancements.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <BatchDocumentUploader />
   </StrictMode>,
 );
